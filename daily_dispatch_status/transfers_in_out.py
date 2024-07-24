8937d150b1925b68f1a069f0a2f0c3e0b2ae877c
poleoks <okuttupolestephen@gmail.com>
2024-07-24 14:14:25 +0300

    24-07-2024

--- daily_dispatch_status/transfers_in_out.py
+++ daily_dispatch_status/transfers_in_out.py
@@ -1,6 +1,6 @@
 #%%
 #import modules
-
+import glob
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -49,58 +49,45 @@ Options=webdriver.ChromeOptions()
 Options.add_experimental_option("detach", True)
 
 driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=Options)
-
-#%%
 # powerbi login url
 driver.get('https://app.powerbi.com/?noSignUpCheck=1')
 driver.maximize_window()
 # wait for page load
 time.sleep(5)
 # find email area using xpath
-
-# email = driver.find_element("xpath",'//*[@id="i0116"]')
-
-email=WebDriverWait(driver,5*60).until(
+email=WebDriverWait(driver, 5*60).until(
     EC.presence_of_element_located((By.XPATH,"//input[@class='form-control ltr_override input ext-input text-box ext-text-box']"))
 )
 # # send power bi login user
 pbi_user='d365@yalelo.ke'
 pbi_pass='Kenya@YK'
 email.send_keys(pbi_user)
-#%% find submit button
-submit = WebDriverWait(driver,5*60).until(
-    EC.presence_of_element_located((By.ID,'idSIButton9'))
-)
+
+#find submission data
+submit = driver.find_element("id",'idSIButton9')
 # # click for submit button
 submit.click()
 
-
-#%%
+time.sleep(5)
 # now we need to find password field
-password = WebDriverWait(driver,5*60).until(
-    EC.presence_of_element_located((By.ID,'i0118'))
+password = WebDriverWait(driver, 5*60).until(
+    EC.presence_of_element_located(("id",'i0118'))
 )
 # then we send our user's password 
 password.send_keys(pbi_pass)
 # after we find sign in button above
-submit = WebDriverWait(driver,5*60).until(
-    EC.presence_of_element_located((By.ID,'idSIButton9'))
-)
+submit = driver.find_element("id",'idSIButton9')
 # then we click to submit button
 submit.click()
 # time.sleep(3)
 time.sleep(5)
 # to complate the login process we need to click no button from above
 # find no button by element
-no = WebDriverWait(driver,5*60).until(
-    EC.presence_of_element_located((By.ID,'idBtn_Back'))
-)
+no = driver.find_element("id",'idBtn_Back')
 # then we click to no
 no.click()
 time.sleep(5)
 # //*[@id="email"]
-
-#%%
 #navigate the report and take screenshot
 current_dir= "P:/Pertinent Files/Python/scripts/daily_dispatch_status/"
 ss_name ='distribution.png'
@@ -108,58 +95,71 @@ ss_path=current_dir+ss_name
 # ss_path = os.path.join(current_dir, "/", ss_name)
 # print(f"This is the picture path: {ss_path}")
 
-report_url="https://app.powerbi.com/groups/6514fc4d-2ddc-4df0-8cd7-1a6a5f7deed8/reports/74da8449-897f-467a-be26-56a067be3b0c/a9add40f90705eba05c8"
+transfer_in_url="https://app.powerbi.com/groups/6514fc4d-2ddc-4df0-8cd7-1a6a5f7deed8/reports/74da8449-897f-467a-be26-56a067be3b0c/e4e07d5f73507b70b7bd"
+transfer_out_url="https://app.powerbi.com/groups/6514fc4d-2ddc-4df0-8cd7-1a6a5f7deed8/reports/74da8449-897f-467a-be26-56a067be3b0c/203e64e3a7c660b4d76a"
 
 # Navigate to the report URL
-driver.get(report_url)
+driver.get(transfer_in_url)
 # wait for report load
 
-#%%
-#wait for a max of 5 mins until full load
-# filter shipping warehouse
-# Locate and click the dropdown to make the items visible
-dropdown = WebDriverWait(driver, 5*60).until(
-        EC.presence_of_element_located((By.XPATH,'//*[@class ="slicer-dropdown-menu" and @aria-label="ShippingWarehouseId"]'))
+#Click to get Drop Down Menu
+download_address=glob.glob("C:/Users/Pole Okuttu/Downloads/data"+ "*xlsx")
+
+def export_tr(url):
+    file_path=[]
+    for h in download_address:
+        os.remove(h)
+
+# load url
+    driver.get(url)
+    hover_element=WebDriverWait(driver, 5*60).until(
+        EC.presence_of_element_located((By.XPATH,'//*[ @role="presentation" and @class="top-viewport"]'))
     )
-dropdown.click()
-# Add a delay to ensure the dropdown menu is fully loaded
-time.sleep(5)  # You may need to adjust the sleep duration based on the page load speed
-    # Wait for the search box to be present within the dropdown
 
+    ActionChains(driver).double_click(hover_element).perform()
 
-ActionChains(dropdown).send_keys(Keys.CONTROL, "f").perform()
+    WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@data-testid="visual-more-options-btn" and @class="vcMenuBtn" and @aria-expanded="false" and @aria-label="More options"]'))
+        ).click()
 
-# def scroll_within_container(container_xpath, element_xpath):
-#     # Locate the container
-#     container = driver.find_element(By.XPATH, container_xpath)
-    
-#     # Scroll until the element is visible
-#     while True:
-#         try:
-#             time.sleep(5)
-#             # Try to find the element within the container
-#             element = container.find_element(By.XPATH, element_xpath)
-#             if element.is_displayed():
-#                 return element
-#         except:
-#             pass  # Element not found, continue scrolling
+    #Click Export data
+    WebDriverWait(driver, 3*60).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@title="Export data"]'))
+    ).click()
 
-#         # Scroll the container down
-#         driver.execute_script("arguments[0].scrollBy(0, 100);", container)
-#         time.sleep(2)  # Wait for the scroll to complete
+    #Click Export Icon
+    WebDriverWait(driver, 3*60).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@aria-label="Export"]'))
+    ).click()
 
-# # Define the XPath for the container and the element
-# container_xpath = "//div[@class='scrollable-container']"  # Replace with your container's XPath
-# element_xpath = "//div[@class='slicerItemContainer' and @title='Swift']"
+    # Wait for the file to download (this might need adjustments based on download time)
+    WebDriverWait(driver, 3*60).until(
+        lambda driver: len(glob.glob("C:/Users/Pole Okuttu/Downloads/data*.xlsx")) > len(file_path)
+    )
 
-# # Scroll within the container until the element is visible
-# element = scroll_within_container(container_xpath, element_xpath)
+    # Get the latest downloaded file
+    tr_file = pd.read_excel('C:/Users/Pole Okuttu/Downloads/data.xlsx')
+    # Close the browser
+    return tr_file
 
 
 
-# # Perform actions with the element if needed
-# print("Element found and is visible:", element.text)
-time.sleep(10)
-# Close the browser
-driver.quit()
-# #%%
+#%%
+#Read
+
+transfers_in = export_tr(transfer_in_url)
+transfers_out = export_tr(transfer_out_url)
+
+#%%
+# df_in = transfers_in.pivot(index=["Date"], columns=["ProductSizeId"], values="Received (kg)")
+df_in = pd.pivot_table(transfers_in,index="Date", columns=["SKU","ProductSizeId"], values="Received (kg)", aggfunc='sum')
+df_in['Total'] = df_in.sum(axis=1, skipna=True)
+df_in.head()
+
+#%%
+
+# df_in = transfers_in.pivot(index=["Date"], columns=["ProductSizeId"], values="Received (kg)")
+df_out = pd.pivot_table(transfers_out,index="Date", columns=["SKU","ProductSizeId"], values="Received (kg)", aggfunc='sum')
+df_out['Total'] = df_out.sum(axis=1, skipna=True)
+df_out.head()
+# %%
