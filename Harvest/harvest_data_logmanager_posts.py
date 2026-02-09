6c20ba06c24ef1227b71c4e060949ed6f3e23464
poleoks <poleoks@gmail.com>
2026-02-09 13:57:03 +0300

    9.2.2026

--- Harvest/harvest_data_logmanager_posts.py
+++ Harvest/harvest_data_logmanager_posts.py
@@ -49,11 +49,12 @@ my_cursor=conn.cursor()
 
 # SQL query to select top 5 rows
 sql = "SELECT * FROM productionlogsheet lg where DATE(lg.datetime) = CURRENT_DATE"#-1"
+# sql  = "SHOW DATABASES"
 my_cursor.execute(sql)
 
 # Fetch the results
 df_mysql = my_cursor.fetchall()
-# print(df_mysql)
+print(df_mysql)
 col_names=[i[0] for i in my_cursor.description]
 # Print the df_mysql
 # for row in df_mysql:
@@ -80,3 +81,21 @@ else:
 print(f"Total Weight: {total_weight:.2f} kg")
 print(f"Total Hours: {total_hours:.2f}")
 print(f"Ton/Hr: {avg_weight_per_hour:.2f}")
+
+
+#%%
+# # check tables
+# import mysql.connector
+
+# # Execute the command
+# my_cursor.execute("SHOW TABLES")
+
+# # Fetch all results
+# tables = my_cursor.fetchall()
+
+# print("Tables in the database:")
+# for table in tables:
+#     my_cursor.execute(f"SELECT COUNT(*) FROM {table[0]}")
+#     count = my_cursor.fetchone()[0]
+#     print(f"- {table[0]}: {count} records")
+#     print(f"- {table[0]}")
