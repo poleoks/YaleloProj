821f7717933060a594162747d1a234c9025ca71c
poleoks <poleoks@gmail.com>
2026-03-12 11:45:46 +0300

    InventShp

--- Finance/InventTransNew_shipments.py
+++ Finance/InventTransNew_shipments.py
@@ -56,7 +56,7 @@ print("grid clicked")
 
 
 clk=WebDriverWait(browser,15).until(
-                EC.presence_of_element_located((By.XPATH,'//*[@aria-label="Insert columns..."]'))
+                EC.presence_of_element_located((By.XPATH,'(//*[contains(@class,"dyn-button-label")])[1]'))
                 )
 print("click!")
 clk.click()
